66a02c5d3d1627dbb607be582a00b952a595a0ac
you21979 <you2197901@gmail.com>
2014-03-03 17:00:24 +0900

    add jsonread

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 exports.create = require('./lib/main');
 exports.Graph = require('./lib/graph');
 exports.Model = require('./lib/model');
+var fs = require('fs');
+exports.jsonFileRead = function(filename, callback){
+    fs.readFile(filename, 'utf-8', function(err, val){
+        if(err){
+            return callback(err, val);
+        }
+        try{
+            var j = JSON.parse(val);
+            callback(null, j);
+        }catch(err){
+            callback(err, val);
+        }
+    });
+};
